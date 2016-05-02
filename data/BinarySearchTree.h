d5892ede5bd980a6b21fbbbb72ef593d4b15d3ad
Rohan Ramasamy <Rohan@Rohans-MacBook-Pro.local>
2016-05-02 22:16:10 +0100

    #4 Implemented insert and search functions for BST

--- data/BinarySearchTree.h
+++ data/BinarySearchTree.h
@@ -9,6 +9,7 @@ This file contains the code for a binary search tree.
 
 #include <utility>
 #include <vector>
+#include <cstddef>
 
 #include <iostream>
 
@@ -19,7 +20,8 @@ namespace {
 template<class key, class data>
 class Node {
 public:
-	Node(key keyValue, data dataValue) : mLeftInitialised(false), mRightInitialised(false) {
+	Node(key keyValue,
+		data dataValue) : mRight(nullptr), mLeft(nullptr) {
 		std::vector<data> tmpData = { dataValue };
 		mKey = std::pair<key, std::vector<data> >(keyValue, tmpData);
 	}
@@ -32,38 +34,113 @@ public:
 
 	Node*
 	right() { 
-		if (mRightInitialised)	{ return mRight; }
-		else { throw std::runtime_error("Null Pointer"); }
+		return mRight;
 	}
 
 	Node*
 	left() { 
-		if (mLeftInitialised) { return mLeft; }
-		else { throw std::runtime_error("Null Pointer"); }
+		return mLeft;
 	}
 
 	void
-	setRight(Node* newPtr) { 
-		if (!mRightInitialised) { mRightInitialised = true; }
+	setRight(Node* newPtr) {
 		mRight = newPtr;
 	}
 
 	void
 	setLeft(Node* newPtr) {
-		if (!mLeftInitialised) { mLeftInitialised = true; }
 		mLeft = newPtr;
 	}
 
 	void
 	addData(data newData) { mKey.second.push_back(newData); }
+
+	void
+	addData(std::vector<data> newDataVector) {
+		for (auto newData : newDataVector) {
+			mKey.second.push_back(newData);
+		}
+	}
 private:
 	std::pair<key, std::vector<data> > mKey;
 	Node* mRight;
 	Node* mLeft;
-	bool mLeftInitialised;
-	bool mRightInitialised;
 };
 
 }
+
+template<class key, class data>
+class BinarySearchTree {
+	typedef Node<key, data> Node;
+public:
+	BinarySearchTree() : mRoot(nullptr) {}
+
+	BinarySearchTree(key rootKey, data rootData) {
+		mRoot = new Node(rootKey, rootData);
+	}
+
+	BinarySearchTree(Node rootNode) {
+		mRoot = &rootNode;
+	}
+
+	void
+	insert(Node& newNode) {
+		if (mRoot == NULL) {
+			mRoot = &newNode; 
+		}
+		else if (mRoot->getKey() > newNode.getKey()) {
+			insert(mRoot->left(), newNode);
+		}
+		else if (mRoot->getKey() < newNode.getKey()) {
+			insert(mRoot->right(), newNode);
+		}
+		else {
+			mRoot->addData(newNode.getData());
+		}
+	}
+
+	void
+	insert(Node* parentNode,
+		   Node& newNode) {
+		if (parentNode == NULL) {
+		    parentNode = &newNode; 
+		}
+		else if (parentNode->getKey() > newNode.getKey()) {
+			insert(parentNode->left(), newNode);
+		}
+		else if (parentNode->getKey() < newNode.getKey()) {
+			insert(parentNode->right(), newNode);
+		}
+		else {
+			parentNode->addData(newNode.getData());
+		}
+	}
+
+	bool
+	search(key searchKey) {
+		if (mRoot == NULL) { return false; }
+		else if (mRoot->getKey() > searchKey) { search(mRoot->left(), searchKey); }
+		else if (mRoot->getKey() < searchKey) { search(mRoot->right(), searchKey); }
+		else { return true; }
+	}
+
+	bool
+	search(Node* node,
+		   key searchKey) {
+		if (node == NULL) { return false; }
+		else if (node->getKey() > searchKey) { search(node->left(), searchKey); }
+		else if (node->getKey() < searchKey) { search(node->right(), searchKey); }
+		else { return true; }
+	}
+
+	// void
+	// insertData(key searchKey,
+	// 	       data newData);
+
+	// void
+	// delete(key searchKey);
+private:
+	Node* mRoot;
+};
 
 }
--- testing/BinarySearchTree_unittest.cpp
+++ testing/BinarySearchTree_unittest.cpp
@@ -8,8 +8,6 @@ This file contains unit tests for BinarySearchTree
 // Linked Includes
 #include "gtest/gtest.h"
 
-#include <iostream>
-
 // Project Includes
 #include "../data/BinarySearchTree.h"
 
@@ -23,13 +21,18 @@ TEST_F(BinarySearchTreeTest, NodeTest) {
 	EXPECT_EQ(1, node1.getKey());
 	EXPECT_EQ(3.0, node1.getData().at(0));
 
-	EXPECT_ANY_THROW(node1.right());
-	EXPECT_ANY_THROW(node1.left());
-
 	node1.addData(2.0);
 	EXPECT_EQ(3.0, node1.getData().at(0));
 	EXPECT_EQ(2.0, node1.getData().at(1));
 	EXPECT_ANY_THROW(node1.getData().at(2));
+
+	std::vector<double> dataVector = { 3.0, 4.0 };
+	node1.addData(dataVector);
+	EXPECT_EQ(3.0, node1.getData().at(0));
+	EXPECT_EQ(2.0, node1.getData().at(1));
+	EXPECT_EQ(3.0, node1.getData().at(2));
+	EXPECT_EQ(4.0, node1.getData().at(3));
+	EXPECT_ANY_THROW(node1.getData().at(4));
 
 	Node<int, double>* rightPtr = new Node<int, double>(3, 5.0);
 	Node<int, double>* leftPtr = new Node<int, double>(3, 5.0);
@@ -43,27 +46,37 @@ TEST_F(BinarySearchTreeTest, NodeTest) {
 	EXPECT_EQ(5.0, node1.left()->getData().at(0));
 }
 
-// TEST_F(BinarySearchTreeTest, InsertionTest) {
-// 	BinarySearchTree<double, double> bST();
+TEST_F(BinarySearchTreeTest, InsertionTest) {
+	BinarySearchTree<double, double> bST(Node<double, double>(1.0, 5.0));
 
-// 	EXPECT_NO_THROW(bST.insert(5));
-// 	EXPECT_NO_THROW(bST.insert(1));
-// 	EXPECT_NO_THROW(bST.insert(3));
-// 	EXPECT_NO_THROW(bST.insert(9));
-// }
+	Node<double, double> node1(-1.0, 5.0);
+	Node<double, double> node2(-2.0, 4.0);
+	Node<double, double> node3(2.0, 2.0);
+	Node<double, double> node4(5.0, 5.0);
 
-// TEST_F(BinarySearchTreeTest, SearchTest) {
-// 	BinarySearchTree<int, int> bST();
+	EXPECT_NO_THROW(bST.insert(node1));
+	EXPECT_NO_THROW(bST.insert(node2));
+	EXPECT_NO_THROW(bST.insert(node3));
+	EXPECT_NO_THROW(bST.insert(node4));
+	EXPECT_NO_THROW(bST.insert(node4));
+}
 
-// 	bST.insert(std::pair<int, int>(5, 3));
-// 	bST.insert(1);
-// 	bST.insert(4);
+TEST_F(BinarySearchTreeTest, SearchTest) {
+	BinarySearchTree<double, double> bST;
 
-// 	EXPECT_TRUE(bST.search(5));
-// 	EXPECT_FALSE(bST.search(0));
+	Node<double, double> node1(-1.0, 5.0);
+	Node<double, double> node2(-2.0, 4.0);
+	Node<double, double> node3(2.0, 2.0);
 
-// 	EXPECT_EQ(3, bST.find(5));
-// }
+	bST.insert(node1);
+	bST.insert(node2);
+	bST.insert(node3);
+
+	EXPECT_TRUE(bST.search(-1.0));
+	EXPECT_FALSE(bST.search(6.0));
+
+	// EXPECT_EQ(2.0, bST.find(2.0));
+}
 
 // TEST_F(BinarySearchTreeTest, DeletionTest) {
 // 	BinarySearchTree<int, int> bST();
