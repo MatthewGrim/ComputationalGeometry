8a1791b53ff089b2d135dc3155f2adfcdcec57a1
Rohan Ramasamy <Rohan@Rohans-MacBook-Pro.local>
2016-05-04 18:59:30 +0100

    #4 - Added API for BST

--- data/BinarySearchTree.h
+++ data/BinarySearchTree.h
@@ -7,10 +7,13 @@ This file contains the code for a binary search tree.
 
 #pragma once
 
+// Standard includes
 #include <utility>
 #include <vector>
 #include <cstddef>
+#include <memory>
 
+// Temporary Includes
 #include <iostream>
 
 namespace compGeom {
@@ -73,6 +76,7 @@ template<class key, class data>
 class BinarySearchTree {
 	typedef Node<key, data> Node;
 public:
+	// Constructors
 	BinarySearchTree() : mRoot(nullptr) {}
 
 	BinarySearchTree(const key& rootKey,
@@ -84,6 +88,10 @@ public:
 		mRoot = &rootNode;
 	}
 
+	/*
+	@description: Inserts a new node into the binary search tree.
+	@param newNode: node to be added to the binary search tree
+	*/
 	void
 	insertNode(const Node& newNode) {
 		if (mRoot == NULL) {
@@ -100,6 +108,83 @@ public:
 		}
 	}
 
+	/*
+	@description: searches to see if a particular key exists within the binary search tree.
+	@param searchKey: the key to be searched for within the tree.
+	*/
+	bool
+	search(const key& searchKey) const {
+		if (mRoot == NULL) { return false; }
+		else if (mRoot->getKey() > searchKey) {return search(mRoot->left(), searchKey); }
+		else if (mRoot->getKey() < searchKey) {return search(mRoot->right(), searchKey); }
+		else { return true; }
+	}
+
+	/*
+	@description: gets the data for a particular key value. It is assumed that the key is
+				  known to exist and so the funtion throws an error if it does not.
+	@param searchKey: the corresponding to the data being retrieved.
+	*/
+	std::vector<data>
+	find(const key& searchKey) const {
+		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
+		else if (mRoot->getKey() > searchKey) {return find(mRoot->left(), searchKey); }
+		else if (mRoot->getKey() < searchKey) {return find(mRoot->right(), searchKey); }
+		else { return mRoot->getData(); }
+	}
+
+	/*
+	@description: inserts additional data into an existing node.
+	@param searchKey: the key for the node to be added to.
+	@param newData: the singular data to be added to the node.
+	*/
+	void
+	insertData(const key& searchKey,
+		       const data& newData) {
+		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
+		else if (mRoot->getKey() > searchKey) {return insertData(mRoot->left(), searchKey, newData); }
+		else if (mRoot->getKey() < searchKey) {return insertData(mRoot->right(), searchKey, newData); }
+		else { return mRoot->addData(newData); }
+	}
+
+	/*
+	@description: inserts additional data into an existing node.
+	@param searchKey: the key for the node to be added to.
+	@param newData: the vector of data to be added to the node.
+	*/
+	void
+	insertData(const key& searchKey,
+		       const std::vector<data>& newData) {
+		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
+		else if (mRoot->getKey() > searchKey) {return insertData(mRoot->left(), searchKey, newData); }
+		else if (mRoot->getKey() < searchKey) {return insertData(mRoot->right(), searchKey, newData); }
+		else { return mRoot->addData(newData); }
+	}
+
+	/*
+	@description: removes a node and any existing children nodes from the data structure.
+	@param searchKey: the key of the node to be deleted should it exist.
+	*/
+	void
+	deleteNode(const key& searchKey) {
+		if (mRoot == NULL) { }
+		else if (mRoot->getKey() > searchKey) {deleteNode(mRoot->left(), searchKey); }
+		else if (mRoot->getKey() < searchKey) {deleteNode(mRoot->right(), searchKey); }
+		else { 
+			deleteNode(mRoot->left());
+			deleteNode(mRoot->right());
+			delete mRoot;
+			mRoot = nullptr;
+		}
+	}
+
+private:
+	Node* mRoot;
+	
+	/*
+	Private implementations of API functions that are used to act on children nodes within
+	the data hierarchy of the binary search tree.
+	*/
 	void
 	insertNode(Node*& parentNode,
 		   const Node& newNode) {
@@ -114,28 +199,12 @@ public:
 	}
 
 	bool
-	search(const key& searchKey) const {
-		if (mRoot == NULL) { return false; }
-		else if (mRoot->getKey() > searchKey) {return search(mRoot->left(), searchKey); }
-		else if (mRoot->getKey() < searchKey) {return search(mRoot->right(), searchKey); }
-		else { return true; }
-	}
-
-	bool
 	search(Node* node,
 		   const key& searchKey) const {
 		if (node == NULL) { return false; }
 		else if (node->getKey() > searchKey) { return search(node->left(), searchKey); }
 		else if (node->getKey() < searchKey) { return search(node->right(), searchKey); }
 		else { return true; }
-	}
-
-	std::vector<data>
-	find(const key& searchKey) const {
-		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
-		else if (mRoot->getKey() > searchKey) {return find(mRoot->left(), searchKey); }
-		else if (mRoot->getKey() < searchKey) {return find(mRoot->right(), searchKey); }
-		else { return mRoot->getData(); }
 	}
 
 	std::vector<data>
@@ -148,15 +217,6 @@ public:
 	}
 
 	void
-	insertData(const key& searchKey,
-		       const data& newData) {
-		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
-		else if (mRoot->getKey() > searchKey) {return insertData(mRoot->left(), searchKey, newData); }
-		else if (mRoot->getKey() < searchKey) {return insertData(mRoot->right(), searchKey, newData); }
-		else { return mRoot->addData(newData); }
-	}
-
-	void
 	insertData(Node* node,
 			   const key& searchKey,
 		       const data& newData) {
@@ -167,15 +227,6 @@ public:
 	}
 
 	void
-	insertData(const key& searchKey,
-		       const std::vector<data>& newData) {
-		if (mRoot == NULL) { throw std::runtime_error("Invalid Search Key"); }
-		else if (mRoot->getKey() > searchKey) {return insertData(mRoot->left(), searchKey, newData); }
-		else if (mRoot->getKey() < searchKey) {return insertData(mRoot->right(), searchKey, newData); }
-		else { return mRoot->addData(newData); }
-	}
-
-	void
 	insertData(Node* node,
 			   const key& searchKey,
 		       const std::vector<data>& newData) {
@@ -183,30 +234,6 @@ public:
 		else if (node->getKey() > searchKey) {return insertData(node->left(), searchKey, newData); }
 		else if (node->getKey() < searchKey) {return insertData(node->right(), searchKey, newData); }
 		else { return node->addData(newData); }
-	}
-
-	void
-	deleteNode(Node*& node) {
-		if (node == NULL) { }
-		else {
-			if (node->left()) { deleteNode(node->left()); }
-			if (node->right()) { deleteNode(node->right()); }
-			delete node;
-			node = nullptr;
-		}
-	}
-
-	void
-	deleteNode(const key& searchKey) {
-		if (mRoot == NULL) { }
-		else if (mRoot->getKey() > searchKey) {deleteNode(mRoot->left(), searchKey); }
-		else if (mRoot->getKey() < searchKey) {deleteNode(mRoot->right(), searchKey); }
-		else { 
-			deleteNode(mRoot->left());
-			deleteNode(mRoot->right());
-			delete mRoot;
-			mRoot = nullptr;
-		}
 	}
 
 	void
@@ -222,8 +249,17 @@ public:
 			node = nullptr;
 		}
 	}
-private:
-	Node* mRoot;
+
+	void
+	deleteNode(Node*& node) {
+		if (node == NULL) { }
+		else {
+			if (node->left()) { deleteNode(node->left()); }
+			if (node->right()) { deleteNode(node->right()); }
+			delete node;
+			node = nullptr;
+		}
+	}
 };
 
 }
